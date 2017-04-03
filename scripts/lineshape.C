b3b3eda368ba40bc8825ac30e343925b46b4c947
Felix Diel <fdiel@noether.ikp.uni-koeln.de>
2017-04-03 11:41:07 +0200

    New version of the lineshape correction. Also includes position dependence and options for Theta and Phi angles.

--- scripts/lineshape.C
+++ scripts/lineshape.C
@@ -2,15 +2,8 @@
 
 #include "TMatrixD.h"
 
-void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
-                int x_o) {
-
-  // double peak_pos = 448.0;
-  // double peak_width = 2.0;
-
-  // double lo_th = -1.9;
-  // double hi_th = 1.9;
-  // double steps = 100.0;
+void line_shape(TTree *tree, std::string b_opt, std::string opt,
+                std::string inp_f, int th_o, int x_o) {
 
   TCanvas *c1 = new TCanvas("c2", "Test", 800, 600);
 
@@ -38,6 +31,46 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
 
   double delay = 100000;
 
+  double hist_ang_lo;
+  double hist_ang_hi;
+  std::string par_name;
+  std::string conf_par_name;
+  std::string hist_name;
+
+  TH2F *hist;
+
+  if (b_opt == "-th") {
+
+    hist_ang_lo = -1.1;
+    hist_ang_hi = 1.1;
+    par_name = "thetaSCAT";
+    conf_par_name = "XCOR_TH_PAR";
+    hist_name = "hThSCATvsX1";
+
+    if (hThSCATvsX1) {
+      hist = hThSCATvsX1;
+    } else {
+      hist = new TH2F("hThSCATvsX1", "SCATTERING Theta vs X position", 3200,
+                      -600., 600., 600, -3., 3.);
+    }
+
+  } else if (b_opt == "-ph") {
+    hist_ang_lo = -3.0;
+    hist_ang_hi = 3.0;
+    par_name = "phiSCAT";
+    conf_par_name = "XCOR_PH_PAR";
+    hist_name = "hPhSCATvsX1";
+    if (hPhSCATvsX1) {
+      hist = hPhSCATvsX1;
+    } else {
+      hist = new TH2F("hPhSCATvsX1", "SCATTERING Phi vs X position", 3200,
+                      -600., 600., 600, -4., 4.);
+    }
+  } else {
+    std::cout << "Option not defined: killing process." << std::endl;
+    return;
+  }
+
   std::string canvas_name;
 
   if (myfile.is_open()) {
@@ -51,10 +84,7 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       copy(istream_iterator<std::string>(iss), istream_iterator<std::string>(),
            back_inserter(input));
 
-      // double peakData[input.size() - 2];
-
-      std::cout << "processing line" << std::endl;
-      // shape(tree, cnt_l, opt, input, th_o,c1);
+      // std::cout << "processing line" << std::endl;
 
       peak_pos = stod(input[0]);
       peak_width = stod(input[1]);
@@ -68,9 +98,10 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       TCanvas *c2 = new TCanvas(canvas_name.c_str(), "Test", 800, 600);
 
       c2->Update();
-      tree->Draw("thetaSCAT:X1pos>>hThSCATvsX1", opt.c_str(), "col");
+      tree->Draw((par_name + ":X1pos>>" + hist_name).c_str(), opt.c_str(),
+                 "col");
 
-      hThSCATvsX1->Draw("colz");
+      hist->Draw("colz");
       c2->Update();
 
       gStyle->SetPalette(55);
@@ -80,8 +111,8 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       c2->cd(1);
 
       gPad->Modified();
-      hThSCATvsX1->Draw("COLZ");
-      hThSCATvsX1->GetXaxis()->SetRangeUser(peak_pos - 20, peak_pos + 40);
+      hist->Draw("COLZ");
+      hist->GetXaxis()->SetRangeUser(peak_pos - 20, peak_pos + 40);
       c2->Update();
 
       gSystem->ProcessEvents();
@@ -93,8 +124,8 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       TF1 *gaus = new TF1("func", "gaus(0)", 0, 10);
       gaus->SetParameters(1, peak_pos, peak_width);
 
-      binMin = hThSCATvsX1->GetYaxis()->FindBin(lo_th);
-      binMax = hThSCATvsX1->GetYaxis()->FindBin(hi_th);
+      binMin = hist->GetYaxis()->FindBin(lo_th);
+      binMax = hist->GetYaxis()->FindBin(hi_th);
 
       sliceBinWidth = (binMax - binMin) / steps;
       sliceUserWidth = (hi_th - lo_th) / steps;
@@ -104,8 +135,8 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       double height[(int)steps];
 
       for (int i = 0; i < steps; ++i) {
-        projX = hThSCATvsX1->ProjectionX("px", i * sliceBinWidth + binMin,
-                                         (i + 1) * sliceBinWidth + binMin, "");
+        projX = hist->ProjectionX("px", i * sliceBinWidth + binMin,
+                                  (i + 1) * sliceBinWidth + binMin, "");
 
         projX->GetXaxis()->SetRangeUser(peak_pos - 20, peak_pos + 40);
         projX->Draw();
@@ -115,7 +146,6 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
 
         height[i] = gaus->GetParameter(0);
 
-        // Sometimes errors happen!
         if ((gaus->GetParameter(1)) > -600) {
 
           projX->Draw("");
@@ -127,7 +157,7 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
           gSystem->ProcessEvents();
           gaus->SetParameters(gaus->GetParameter(0), gaus->GetParameter(1),
                               gaus->GetParameter(2));
-          // sleep(0.1);
+
         } else {
           c2->Update();
           x[i] = x[i - 1];
@@ -135,10 +165,7 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
           gSystem->ProcessEvents();
           gaus->SetParameters(height[i - 1], x[i], 2 * peak_width);
         }
-        usleep(delay);
-        if ((int)steps < 50) {
-          usleep(delay);
-        }
+
         // projX->Delete();
       }
 
@@ -163,38 +190,15 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       s->SetLineColor(kGreen);
       s->Draw("SAME");
 
-      // string opt_corr = "thetaSCAT:X1pos-(";
-
       std::vector<double> out_par;
 
       for (int i = 0; i <= th_o; ++i) {
-        // std::cout << "PAR: " <<   polN->GetParameter(i) << std::endl;
         out_par.push_back(polN->GetParameter(i));
 
         cout << out_par[i] << " ";
       }
-
+      std::cout << std::endl;
       out_list.push_back(out_par);
-
-      // opt_corr += ")>>hThSCATvsX1";
-      /*
-       cout << "" << endl;
-
-       cout << opt_corr << endl;
-
-       c1->cd(1);
-       TGraph *gr2 = new TGraph(steps, x, y);
-       gr2->SetLineWidth(2);
-       gr2->SetLineColor(kRed);
-       gr2->Draw("SAME");
-       // gr2->Draw("SAME","AC*");
-       c1->Update();
-
-       // TCanvas *c2 = new TCanvas("c3", "Test", 800, 600);
-
-       tree->Draw(opt_corr.c_str(), opt.c_str(), "col");
-                */
-      // out_list.push_back(output);
 
       input.clear();
 
@@ -204,18 +208,16 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
   }
 
   // generate the vector for position dependence calculation
-  int line_points = 1002;
+  int line_points = 3001;
   // here for now
-  double range = 1.1;
+  double range = hist_ang_hi;
 
   double step_ang_width = 2 * range / line_points;
-  double step_ang = -range;
+  double step_ang = hist_ang_lo;
 
   std::vector<double> xfp;
   std::vector<double> dxfp;
   std::vector<double> thtgt;
-
-  // cout << range << endl;
 
   int data_cnt = 0;
 
@@ -224,11 +226,8 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
 
   for (unsigned i = 0; i < out_list.size(); ++i) {
 
-    // ptr = (*lineData)[i];
-
     step_ang = -range;
 
-    // cout << ptr[0] << endl;
     for (int j = 0; j < line_points; ++j) {
 
       step_ang += step_ang_width;
@@ -246,9 +245,6 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       }
       dxfp_tmp *= (-1);
 
-      // std::cout << step_ang << " " << xfp_tmp << " " << dxfp_tmp <<
-      // std::endl;
-
       xfp.push_back(xfp_tmp);
       dxfp.push_back(dxfp_tmp);
       thtgt.push_back(step_ang);
@@ -257,7 +253,7 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
     }
   }
 
-  std::cout << "DATA COUNT: " << data_cnt << std::endl;
+  // std::cout << "DATA COUNT: " << data_cnt << std::endl;
 
   // calculate the parameters
 
@@ -267,25 +263,17 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
   int order_x2 = (ang_order + 1) * (x_order + 1);
   int order_x3 = (x_order + 1) * ang_order;
 
-  std::cout << "TEST  " << order_x2 << " " << order_x3 << std::endl;
+  // std::cout << "TEST  " << order_x2 << " " << order_x3 << std::endl;
 
   TMatrixD param(order_x3, order_x3);
-  //= new TMatrixD
+
   TMatrixD param_inv(order_x3, order_x3);
-  ;
-  //= new TMatrixD
 
-  TMatrixD vec(order_x3, 1);   // = new TMatrixD
-  TMatrixD par_v(order_x3, 1); // = new TMatrixD
+  TMatrixD vec(order_x3, 1);
+  TMatrixD par_v(order_x3, 1);
 
-  vec.Print();
-
-  // std::cout << vec[0][0] << std::endl;
-
-  // please dont use orders higher than 9
-
+  // vec.Print();
   std::vector<std::vector<double>> element;
-  //[order_x3][order_x2] = {0.0};
 
   std::vector<double> tmp_row;
 
@@ -297,13 +285,6 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
   }
 
   std::array<double, 81> par = {0.0};
-  // std::array<std::array<double, 81>, 81> element;
-  // for (int dim = 0; dim < 81; ++dim) {
-  // std::array<double, 81> tmp_arr = {0.0};
-  // element[dim] = {0.0};
-  //}
-
-  // double element[order_x3][order_x2] = {0.0};
 
   int l = 0;
   int m = 0;
@@ -325,11 +306,6 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
       tho.push_back(tmp_row_2);
     }
 
-    // nt xo[order_x3][order_x3] = {0};
-    // int tho[order_x3][order_x3] = {0};
-
-    // std::cout << thtgt[i] << " " << xfp[i] << " " << dxfp[i] << endl;
-
     for (int c = 0; c <= x_order; ++c) {
       for (int b = 1; b <= ang_order; ++b) {
         m = 0;
@@ -339,11 +315,6 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
             xo[m][l] = c + c2;
             tho[m][l] = b + b2;
             element[m][l] += pow(thtgt[i], tho[m][l]) * pow(xfp[i], xo[m][l]);
-            // std::cout << thtgt[i] << " " << tho[m][l] << " " << xfp[i] << " "
-            //      << xo[m][l] << std::endl;
-
-            // std::cout << m << " " << l << " " << xo[m][l] << " " << tho[m][l]
-            //     << " " << element[m][l] << std::endl;
             ++m;
           }
         }
@@ -356,8 +327,8 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
     for (int c2 = 0; c2 <= x_order; ++c2) {
       for (int b2 = 1; b2 <= ang_order; ++b2) {
         vec[m] += dxfp[i] * pow(thtgt[i], b2) * pow(xfp[i], c2);
-        cout << m << " " << dxfp[i] << " " << thtgt[i] << "" << b2 << " "
-             << xfp[i] << " " << c2 << " " << vec[m][0] << std::endl;
+        // cout << m << " " << dxfp[i] << " " << thtgt[i] << "" << b2 << " "
+        //     << xfp[i] << " " << c2 << " " << vec[m][0] << std::endl;
         ++m;
       }
     }
@@ -366,17 +337,10 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
   for (int i = 0; i < order_x3; ++i) {
     for (int j = 0; j < order_x3; ++j) {
       param(i, j) = element[i][j];
-      // cout << i << " " << j << " " << element[i][j] << endl;
-      //      element[i][j]=0.0;
     }
   }
 
   param_inv = param.Invert();
-  //  param_inv = param.inv2();
-
-  /*if(!param_inv.zero_chk()){
-          std::cerr << "ERROR : No inverse matrix" << std::endl; exit(1);
-  }*/
 
   par_v.Mult(param_inv, vec);
 
@@ -385,29 +349,18 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
     par[i] = par_v[i][0];
   }
 
-  string opt_corr = "thetaSCAT:X1pos+(";
+  string opt_corr = par_name + ":X1pos+(";
 
   std::vector<double> out_par;
 
-  //%*s %d %d %lf",&global_xcor_th_tho,&global_xcor_th_xo,&global_xcor_th_par);
-
   for (int j = 0; j <= x_o; ++j) {
     for (int i = 1; i <= th_o; ++i) {
-      // cout << i << " " << j << " " << par[j*ang_order + (i-1)]<< endl;
-      // value += par[j*ang_order + (i-1)] * pow(x, i)
-      //			  * pow(y, j);
-      // std::ostringstream tmp_num;
 
-      // tmp_num << std::setprecision(15) << par[j * ang_order + (i - 1)];
-
-      std::cout << "XCOR_TH_PAR"
-                << " " << i << " " << j << " " << par[j * ang_order + (i - 1)]
-                << std::endl;
+      std::cout << conf_par_name << " " << i << " " << j << " "
+                << par[j * ang_order + (i - 1)] << std::endl;
 
       opt_corr += "(" + to_string(par[j * ang_order + (i - 1)]) + ")*(X1pos**" +
-                  to_string(j) + ")*(thetaSCAT**" + to_string(i) + ")";
-
-      // tmp_num.flush();
+                  to_string(j) + ")*(" + par_name + "**" + to_string(i) + ")";
 
       if (j < x_o || i < th_o) {
         opt_corr += " + ";
@@ -415,21 +368,7 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
     }
   }
 
-  /*for (int i = 0; i <= x_o; ++i) {
-    for (int j = 0; j < th_o; ++j) {
-      // out_par.push_back(polN->GetParameter(i));
-      // if (i > 0) {
-      std::cout << j << " " << i << " " << i * th_o + j << " "
-                << (*par_v)[i * th_o + j][0] << std::endl;
-
-
-      // cout << polN->GetParameter(i) << " ";
-    }
-  }*/
-
-  // out_list.push_back(out_par);
-
-  opt_corr += ")>>hThSCATvsX1";
+  opt_corr += ")>>" + hist_name;
 
   cout << "" << endl;
 
@@ -446,6 +385,4 @@ void line_shape(TTree *tree, std::string opt, std::string inp_f, int th_o,
   TCanvas *c0 = new TCanvas("c0", "Test", 800, 600);
 
   tree->Draw(opt_corr.c_str(), opt.c_str(), "col");
-
-  // out_list.push_back(output);
 }
